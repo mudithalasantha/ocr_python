02901b0f27e57feab53fd23f38f14c1fe14f6350
mudithalasantha <mudithalasantha@gmail.com>
2018-07-12 11:23:26 +0530

    changes

--- Dockerfile
+++ Dockerfile
@@ -1,38 +1,41 @@
 # start with a base image
-FROM ubuntu:14.04
+FROM ubuntu:latest
 
 # install dependencies
 RUN apt-get update
-RUN apt-get install -y autoconf automake libtool
-RUN apt-get install -y libpng12-dev
-RUN apt-get install -y libjpeg62-dev
-RUN apt-get install -y g++
-RUN apt-get install -y libtiff4-dev
-RUN apt-get install -y libopencv-dev libtesseract-dev
-RUN apt-get install -y git
-RUN apt-get install -y cmake
-RUN apt-get install -y build-essential
-RUN apt-get install -y libleptonica-dev
-RUN apt-get install -y liblog4cplus-dev
-RUN apt-get install -y libcurl3-dev
-RUN apt-get install -y python2.7-dev
-RUN apt-get install -y tk8.5 tcl8.5 tk8.5-dev tcl8.5-dev
-RUN apt-get --yes install python-imaging
-RUN apt-get install -y imagemagick
-RUN apt-get install -y wget
+RUN apt-get install -y tesseract-ocr
+RUN apt-get install -y libjpeg-dev
+RUN apt-get install -y libpng-dev
+# RUN apt-get install -y autoconf automake libtool
+# RUN apt-get install -y libpng12-dev
+# RUN apt-get install -y libjpeg62-dev
+# RUN apt-get install -y g++
+# RUN apt-get install -y libtiff4-dev
+# RUN apt-get install -y libopencv-dev libtesseract-dev
+# RUN apt-get install -y git
+# RUN apt-get install -y cmake
+# RUN apt-get install -y build-essential
+# RUN apt-get install -y libleptonica-dev
+# RUN apt-get install -y liblog4cplus-dev
+# RUN apt-get install -y libcurl3-dev
+# RUN apt-get install -y python2.7-dev
+# RUN apt-get install -y tk8.5 tcl8.5 tk8.5-dev tcl8.5-dev
+# RUN apt-get --yes install python-imaging
+# RUN apt-get install -y imagemagick
+# RUN apt-get install -y wget
 RUN apt-get install -y python python-pip
-RUN apt-get install -y python-opencv
+# RUN apt-get install -y python-opencv
 
 # build leptonica
 #RUN apt-get install libleptonica-dev
-RUN wget http://www.leptonica.com/source/leptonica-1.71.tar.gz
-RUN tar -zxvf leptonica-1.71.tar.gz
-WORKDIR leptonica-1.71/
-RUN ./autobuild
-RUN ./configure
-RUN make
-RUN make install
-RUN ldconfig
+# RUN wget http://www.leptonica.com/source/leptonica-1.71.tar.gz
+# RUN tar -zxvf leptonica-1.71.tar.gz
+# WORKDIR leptonica-1.71/
+# RUN ./autobuild
+# RUN ./configure
+# RUN make
+# RUN make install
+# RUN ldconfig
 WORKDIR /
 RUN ls
 
@@ -43,21 +46,23 @@ RUN pip install -r requirements.txt
 # RUN apt install tesseract-ocr
 #RUN wget https://tesseract-ocr.googlecode.com/files/tesseract-ocr-3.02.02.tar.gz
 #RUN tar -zxvf tesseract-ocr-3.02.02.tar.gz
-ADD ./tesseract-ocr /tesseract-ocr
-WORKDIR tesseract-ocr/
-RUN ./autogen.sh
-RUN ./configure
-RUN make
-RUN make install
-RUN ldconfig
-RUN cd ..
+# ADD ./tesseract-ocr /tesseract-ocr
+# WORKDIR tesseract-ocr/
+# RUN ./autogen.sh
+# RUN ./configure
+# RUN make
+# RUN make install
+# RUN ldconfig
+# RUN cd ..
 
 # download the relevant Tesseract English Language Packages
 #RUN wget https://tesseract-ocr.googlecode.com/files/tesseract-ocr-3.02.eng.tar.gz
 #RUN tar -xf tesseract-ocr-3.02.eng.tar.gz
-RUN pwd
-ADD ./tesseract-ocr/tessdata /usr/local/share/tessdata
-RUN ls /usr/local/share/tessdata
+# RUN pwd
+# ADD ./tesseract-ocr/tessdata /usr/local/share/tessdata
+# RUN ls /usr/local/share/tessdata
+RUN DEBIAN_FRONTEND=noninteractive apt-get install -y tesseract-ocr-ben
+RUN tesseract --list-langs
 
 # update working directories
 ADD ./flask_server /flask_server
--- flask_server/ocr.py
+++ flask_server/ocr.py
@@ -1,63 +1,130 @@
+# -*- coding: utf-8 -*-
 import pytesseract
 import requests
 import re
 import numpy as np
-import cv2
-from matplotlib import pyplot as plt
+# import cv2
+# from cv2 import cv
+# from matplotlib import pyplot as plt
 from PIL import Image
 from PIL import ImageFilter
 from StringIO import StringIO
 import datetime
+import json
+import urllib
 
 def process_image(url):
     image = _get_image(url)
-    # img = cv2.imread('GettyImage.jpg')
+    # urllib.urlretrieve(url, "Sample.jpg")
+    # img = cv2.imread(image)
     image.filter(ImageFilter.SHARPEN)
-    string_val = pytesseract.image_to_string(image)
-    return _validate_output(string_val)
+    string_val_eng = pytesseract.image_to_string(image, lang='eng')
+    string_val_ben = pytesseract.image_to_string(image, lang='ben')
+
+    # find_objects_result = _find_objects()
+    # return find_objects_result
+    return _validate_output(string_val_eng, string_val_ben)
     # px = img[100,100]
-    # img_m = find_objects()
-    # return px
+    # return img_m # json.dumps(img_m)
 
 def _get_image(url):
     return Image.open(StringIO(requests.get(url).content))
 
-def _validate_output(val):
-    nid = re.search("[0-9]{6,17}", val)
-    nid_string = re.search("NATIONAL ID CARD", val)
-    gprb_1_string = re.search("Government", val)
-    gprb_2_string = re.search("People's", val)
-    gprb_3_string = re.search("Republic", val)
-    gprb_4_string = re.search("Bangladesh", val)
-    dob_string = re.search("Date of Birth", val)
-    name_string = re.search("Name", val)
-    accuracy = _calculate_accuracy(nid, [nid_string, gprb_1_string, gprb_2_string, gprb_3_string, gprb_4_string, dob_string, name_string])
-    name = _retrieve_name(val)
-    blood_group = _retrieve_blood_group(val)
-    dob = _retrieve_dob(val)
-    if nid:
-        return {"raw_data":val, "nid":nid.group(0), "name":name, "blood_group":blood_group, "dob":dob, "accuracy": accuracy, "code": 200, "error": False}
-    else:
-        return {"raw_data":val, "nid":None, "name":name, "blood_group":blood_group, "dob":dob, "accuracy": accuracy, "code": 200, "error": True}
+def _validate_output(val_eng, val_ben):
+    nid = re.search("[0-9]{6,17}", val_eng)
+    nid_string = re.search("NATIONAL ID CARD", val_eng)
+    gprb_1_string = re.search("Government", val_eng)
+    gprb_2_string = re.search("People's", val_eng)
+    gprb_3_string = re.search("Republic", val_eng)
+    gprb_4_string = re.search("Bangladesh", val_eng)
+    dob_string = re.search("Date of Birth", val_eng)
+    name_string = re.search("Name", val_eng)
 
-def _retrieve_name(val):
-    name = re.search("(?<=Name:.).+?(?=\\n)", val)
-    if name:
-        return name.group(0)
+    name_eng, name_ben = _retrieve_name(val_eng, val_ben)
+    blood_group = _retrieve_blood_group(val_eng)
+    dob = _retrieve_dob(val_eng)
+    father = _retrieve_father(val_ben)
+    mother = _retrieve_mother(val_ben)
+
+    accuracy = _calculate_accuracy(nid, 
+    [nid_string, 
+    gprb_1_string, 
+    gprb_2_string, 
+    gprb_3_string, 
+    gprb_4_string, 
+    dob_string, 
+    name_string,
+    name_eng,
+    blood_group,
+    dob
+    ])
+    
+    if nid:
+        return {
+            "raw_data_eng":val_eng, 
+            "raw_data_ben":val_ben, 
+            "nid":nid.group(0) if hasattr(nid, 'group') else None,
+            "name_eng":name_eng.group(0) if hasattr(name_eng, 'group') else None, 
+            "name_ben":name_ben.group(0) if hasattr(name_ben, 'group') else None,
+            "father":father.group(0) if hasattr(father, 'group') else None,
+            "mother":mother.group(0) if hasattr(mother, 'group') else None,
+            "blood_group": blood_group.group(0) if hasattr(blood_group, 'group') else None, 
+            "dob":dob, 
+            "accuracy": accuracy, 
+            "code": 200, 
+            "error": False
+            }
+    else:
+        return {
+            "raw_data_eng":val_eng, 
+            "raw_data_ben":val_ben, 
+            "nid":None, 
+            "name_eng":name_eng.group(0) if hasattr(name_eng, 'group') else None, 
+            "name_ben":name_ben.group(0) if hasattr(name_ben, 'group') else None,
+            "blood_group": blood_group.group(0) if hasattr(blood_group, 'group') else None, 
+            "dob":dob, 
+            "accuracy": accuracy, 
+            "code": 200, 
+            "error": True
+            }
+
+def _retrieve_name(val_eng, val_ben):
+    name_eng = re.search("(?<=Name:.).+?(?=\\n)", val_eng)
+    name_ben = re.search("(?<=নাম:.).+?(?=\\n)", val_ben)
+    if name_eng and name_ben:
+        return name_eng, name_ben
+    elif name_eng:
+        return name_eng, None
+    elif name_ben:
+        return None, name_ben
+    else:
+        return None, None
+
+def _retrieve_father(val):
+    father = re.search("(?<=পিতা:.).+?(?=\\n)", val)
+    if father:
+        return father
+    else:
+        return None
+
+def _retrieve_mother(val):
+    father = re.search("(?<=মাতা:.).+?(?=\\n)", val)
+    if father:
+        return father
     else:
         return None
 
 def _retrieve_blood_group(val):
     b_group = re.search("(?<=Group:.).+?(?=\\n)", val)
     if b_group:
-        return b_group.group(0)
+        return b_group
     else:
         return None
 
 def _retrieve_dob(val):
     dob = re.search("(?<=Birth:.).+?(?=\\n)", val)
     if dob:
-        return validate(dob.group(0))
+        return _validate(dob.group(0))
     else:
         return None
 
@@ -66,143 +133,126 @@ def _calculate_accuracy(nid, param_array):
     single_weight = 70/len(param_array)
     if nid:
         for idx, item in enumerate(param_array):
-            if item:
+            if item and hasattr(item, 'group') and re.match("[\w., _+-:]", str(item.group(0))):
+                value+= single_weight
+            elif item and not hasattr(item, 'group') and re.match("[\w., _+-:]", str(item)):
                 value+= single_weight
         return value
     else:
         return 0
 
-def validate(date_text):
+def _validate(date_text):
     try:
          return datetime.datetime.strptime(date_text, '%d %b %Y').strftime("%d %b %Y")
     except ValueError:
         if date_text[0] in "689":
             temp_list = list(date_text)
             temp_list[0] = '0'
-            return validate("".join(temp_list))
+            return _validate("".join(temp_list))
         if date_text[0] in "7":
             temp_list = list(date_text)
             temp_list[0] = '1'
-            return validate("".join(temp_list))
-        is_month_corrected, return_value = month_correction(date_text)
+            return _validate("".join(temp_list))
+        is_month_corrected, return_value = _month_correction(date_text)
         if is_month_corrected:
             return return_value
-        return "Error in format"
+        return None
 
-def month_correction(date_text):
+def _month_correction(date_text):
     split_list = date_text.split()
     temp_list = list(date_text)
     if "Ja" in split_list[1] or "an" in split_list[1]:
         temp_list[3] = 'J'
         temp_list[4] = 'a'
         temp_list[5] = 'n'
-        return True, validate("".join(temp_list))
+        return True, _validate("".join(temp_list))
     if "Fe" in split_list[1] or "eb" in split_list[1]:
         temp_list[3] = 'F'
         temp_list[4] = 'e'
         temp_list[5] = 'b'
-        return True, validate("".join(temp_list))
-    if "Ma" in split_list[1] or "ar" in split_list[1]:
+        return True, _validate("".join(temp_list))
+    if "ar" in split_list[1]:
         temp_list[3] = 'M'
         temp_list[4] = 'a'
         temp_list[5] = 'r'
-        return True, validate("".join(temp_list))
+        return True, _validate("".join(temp_list))
     if "Ap" in split_list[1] or "pr" in split_list[1]:
         temp_list[3] = 'A'
         temp_list[4] = 'p'
         temp_list[5] = 'r'
-        return True, validate("".join(temp_list))
-    if "Ma" in split_list[1] or "ay" in split_list[1]:
+        return True, _validate("".join(temp_list))
+    if "ay" in split_list[1]:
         temp_list[3] = 'M'
         temp_list[4] = 'a'
         temp_list[5] = 'y'
-        return True, validate("".join(temp_list))
+        return True, _validate("".join(temp_list))
     if "un" in split_list[1]:
         temp_list[3] = 'J'
         temp_list[4] = 'u'
         temp_list[5] = 'n'
-        return True, validate("".join(temp_list))
+        return True, _validate("".join(temp_list))
     if "ul" in split_list[1]:
         temp_list[3] = 'J'
         temp_list[4] = 'u'
         temp_list[5] = 'l'
-        return True, validate("".join(temp_list))
+        return True, _validate("".join(temp_list))
     if "Au" in split_list[1] or "ug" in split_list[1]:
         temp_list[3] = 'A'
         temp_list[4] = 'u'
         temp_list[5] = 'g'
-        return True, validate("".join(temp_list))
+        return True, _validate("".join(temp_list))
     if "Se" in split_list[1] or "ep" in split_list[1]:
         temp_list[3] = 'S'
         temp_list[4] = 'e'
         temp_list[5] = 'p'
-        return True, validate("".join(temp_list))
+        return True, _validate("".join(temp_list))
     if "Oc" in split_list[1] or "ct" in split_list[1]:
         temp_list[3] = 'O'
         temp_list[4] = 'c'
         temp_list[5] = 't'
-        return True, validate("".join(temp_list))
+        return True, _validate("".join(temp_list))
     if "No" in split_list[1] or "ov" in split_list[1]:
         temp_list[3] = 'N'
         temp_list[4] = 'o'
         temp_list[5] = 'v'
-        return True, validate("".join(temp_list))
+        return True, _validate("".join(temp_list))
     if "De" in split_list[1] or "ec" in split_list[1]:
         temp_list[3] = 'D'
         temp_list[4] = 'e'
         temp_list[5] = 'c'
-        return True, validate("".join(temp_list))
+        return True, _validate("".join(temp_list))
     return False, None
 
-# def find_objects():
-#     MIN_MATCH_COUNT = 10
+def _find_objects():
+    # method = cv.CV_TM_SQDIFF_NORMED
 
-#     img1 = cv2.imread('GettyImage.jpg',0)          # queryImage
-#     img2 = cv2.imread('GettyImage.jpg',0) # trainImage
+    # Read the images from the file
+    small_image = cv2.imread('Bangla_logo.jpg')
+    large_image = cv2.imread('Sample.jpg')
 
-#     # Initiate SIFT detector
-#     sift = cv2.SIFT()
+    result = cv2.matchTemplate(small_image, large_image, 1)
+    
+    minVal,maxVal,minLoc,maxLoc = cv2.minMaxLoc(result)
+    MPx,MPy = minLoc
+    trows,tcols = small_image.shape[:2]
+    cv2.rectangle(large_image, (MPx,MPy),(MPx+tcols,MPy+trows),(0,0,255),2)
+    cv2.imwrite('messigray.png',large_image)
+    return minVal
+    # # We want the minimum squared difference
+    # mn,_,mnLoc,_ = cv2.minMaxLoc(result)
 
-#     # find the keypoints and descriptors with SIFT
-#     kp1, des1 = sift.detectAndCompute(img1,None)
-#     kp2, des2 = sift.detectAndCompute(img2,None)
+    # # Draw the rectangle:
+    # # Extract the coordinates of our best match
+    # MPx,MPy = mnLoc
 
-#     FLANN_INDEX_KDTREE = 0
-#     index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-#     search_params = dict(checks = 50)
+    # # Step 2: Get the size of the template. This is the same size as the match.
+    # trows,tcols = small_image.shape[:2]
 
-#     flann = cv2.FlannBasedMatcher(index_params, search_params)
+    # # Step 3: Draw the rectangle on large_image
+    # cv2.rectangle(large_image, (MPx,MPy),(MPx+tcols,MPy+trows),(0,0,255),2)
 
-#     matches = flann.knnMatch(des1,des2,k=2)
+    # # Display the original image with the rectangle around the match.
+    # cv2.imshow('output',large_image)
 
-#     # store all the good matches as per Lowe's ratio test.
-#     good = []
-#     for m,n in matches:
-#         if m.distance < 0.7*n.distance:
-#             good.append(m)
-
-#     if len(good)>MIN_MATCH_COUNT:
-#         src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
-#         dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
-
-#         M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
-#         matchesMask = mask.ravel().tolist()
-
-#         h,w = img1.shape
-#         pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-#         dst = cv2.perspectiveTransform(pts,M)
-
-#         img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
-
-#     else:
-#         matchesMask = None
-#         return "Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT)
-
-#     draw_params = dict(matchColor = (0,255,0), # draw matches in green color
-#                    singlePointColor = None,
-#                    matchesMask = matchesMask, # draw only inliers
-#                    flags = 2)
-
-#     img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
-
-#     return img3
+    # # The image is only displayed if we call this
+    # cv2.waitKey(0)
